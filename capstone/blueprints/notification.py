34fce1d72349298950c737f911cc100d117e4882
heshamzm <47276846+heshamzm@users.noreply.github.com>
2021-02-03 14:29:15 +0200

    created function upgrade request in notifaication

--- capstone/blueprints/notification.py
+++ capstone/blueprints/notification.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, session, flash 
 from flask_wtf import FlaskForm
 from capstone.models.user import User
 from capstone.models.item import Item
-from capstone.models.request import BuyRequest
+from capstone.models.request import BuyRequest , UpgradeRequest
 from capstone.blueprints.user import disable_user , login_required , maintenance
 from bson import ObjectId
 
@@ -24,8 +24,7 @@ def review_buy_request():
             
         my_buy_requests.append(BuyRequest.objects(item = item))
  
-    print('hi00000000')
-    print(my_buy_requests)
+    
 
     return render_template("notifications/review-my-buy-request.html" , my_buy_requests = my_buy_requests)
 
@@ -35,20 +34,102 @@ def review_buy_request():
 @disable_user
 def approve_buy_request(item_id,request_id ):
 
-    current_user = User.objects(id = session['user']['id']).first()
-    my_items = Item.objects(user = current_user)
-    
-    print(my_items)
-    my_buy_requests = []
-    for item in my_items:
-            
-        my_buy_requests.append(BuyRequest.objects(item = item))
+    item = Item.objects(id = item_id).first()
+    item.sold = True
+    Item.objects(id = item_id).update_one(unset__buy_request_list = request_id)
+    item.save()
 
-    print(request_id)
-    # item = Item.objects(id = item_id).first()
-    # item.sold = True
-    # item.save()
+    request = BuyRequest.objects(id = request_id).first()
+    request.status = "Approved"
+    request.save()
+
+    flash("Item has been sold!")
+
+    
     Item.objects(id = item_id).update_one(pull__buy_request_list = request_id)
     
 
-    return render_template("notifications/review-my-buy-request.html" , my_buy_requests = my_buy_requests)
+    return redirect(url_for('notifications.view_buy_request'))
+
+@notification_bp.route('/notification/<item_id>/decline_request/<request_id>', methods=['POST', 'GET'])
+# @maintenance
+# @login_required
+# @disable_user
+def decline_request(item_id,request_id):
+
+   
+    Item.objects(id = item_id).update_one(unset__buy_request_list = request_id)
+
+    request = BuyRequest.objects(id = request_id).first()
+    request.status = "Declined"
+    request.save()
+
+    flash("Buy Request has been Declined!")
+    
+    return redirect(url_for('notifications.view_buy_request'))
+
+
+# upgrade requests functions
+
+@notification_bp.route('/upgrade_request', methods=['POST', 'GET'])
+# @maintenance
+# @login_required
+# @disable_user
+def upgrade_request():
+
+    upgrade_request = UpgradeRequest(user = session['user']['id'], status = "Pending")
+
+    upgrade_request.save()
+
+    flash("Upgrade Request has been sent.")
+
+    return redirect(url_for("profile.profile"))
+
+@notification_bp.route('/review_upgrade_requests', methods=['POST', 'GET'])
+# @maintenance
+# @login_required
+# @disable_user
+def review_upgrade_requests():
+
+    users = User.objects()
+
+    upgrade_requests = []
+
+    for user in users:
+        upgrade_requests.append(UpgradeRequest.objects(user = user).all())
+
+    return render_template("notifications/review_upgrade_requests.html", upgrade_requests = upgrade_requests)
+
+
+@notification_bp.route('/approve_upgrade_request/<request_id>', methods=['POST', 'GET'])
+# @maintenance
+# @login_required
+# @disable_user
+def approve_upgrade_request(request_id):
+
+    request = UpgradeRequest.objects(id = request_id).first()
+    request.status = "Approved"
+    request.save()
+
+    request.user.role = 1
+    request.user.save()
+    
+    flash("Upgrade Request has been approved.")
+
+    return redirect(url_for("notifications.review_upgrade_requests"))
+
+
+@notification_bp.route('/decline_upgrade_request/<request_id>', methods=['POST', 'GET'])
+# @maintenance
+# @login_required
+# @disable_user
+def decline_upgrade_request(request_id):
+
+    request = UpgradeRequest.objects(id = request_id).first()
+    request.status = "Declined"
+    request.save()
+
+    flash("Upgrade Request has been declined.")
+    
+
+    return redirect(url_for("notifications.review_upgrade_requests"))
